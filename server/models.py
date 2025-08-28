f9df022687aee3336bd07a9e2f248570b442ce6e
Abdiaziz77 <abdiaziznoor20@gmail.com>
2025-08-28 07:03:28 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,34 +1,44 @@
+# server/models.py
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
 class Author(db.Model):
-    __tablename__ = 'authors'
-    
+    __tablename__ = "authors"
+
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    name = db.Column(db.String, unique=True, nullable=False)
 
-    # Add validators 
+    posts = db.relationship("Post", backref="author", lazy=True)
 
-    def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value or value.strip() == "":
+            raise ValueError("Author must have a name.")
+        existing_author = Author.query.filter_by(name=value).first()
+        if existing_author:
+            raise ValueError("Author name must be unique.")
+        return value
+
 
 class Post(db.Model):
-    __tablename__ = 'posts'
-    
+    __tablename__ = "posts"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String)
-    category = db.Column(db.String)
-    summary = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    content = db.Column(db.Text, nullable=False)
 
-    # Add validators  
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"), nullable=False)
 
+    @validates("title")
+    def validate_title(self, key, value):
+        if not value or value.strip() == "":
+            raise ValueError("Post must have a title.")
+        return value
 
-    def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    @validates("content")
+    def validate_content(self, key, value):
+        if len(value.strip()) < 50:
+            raise ValueError("Post content must be at least 50 characters long.")
+        return value
